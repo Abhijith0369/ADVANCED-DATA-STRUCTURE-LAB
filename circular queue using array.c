c0c0846907b0d0363826dd57d9f52bb0db0dc183
Abhijith0369 <prakashabhijith408@gmail.com>
2026-01-09 16:36:27 -1200

    Update circular queue using array.c

--- circular queue using array.c
+++ circular queue using array.c
@@ -2,102 +2,92 @@
 
 #define SIZE 5
 
-int Q[SIZE], front = -1, rear = -1;
+int q[SIZE];
+int front = -1, rear = -1;
 
-void enqueue(int item)
-{
-    if ((rear + 1) % SIZE == front)
-    {
-        printf("The queue is full\n");
-    }
-    else
-    {
-        if (front == -1 && rear == -1)
-        {
-            front = 0;
-            rear = 0;
-        }
-        else
-        {
+void enqueue(int item) {
+    if ((rear + 1) % SIZE == front) {
+        printf("Queue is full (Overflow)\n");
+    } else {
+        if (front == -1) {
+            front = rear = 0;
+        } else {
             rear = (rear + 1) % SIZE;
         }
-        Q[rear] = item;
-        printf("%d", item);
+        q[rear] = item;
+        printf("Inserted %d at index %d\n", item, rear);
     }
 }
 
-void dequeue()
-{
-    if (front == -1 && rear == -1)
-    {
-        printf("Queue is empty\n");
-    }
-    else
-    {
-        printf("The deleted item is: %d\n", Q[front]);
-        if (front == rear)
-        {
-            front = -1;
-            rear = -1;
-        }
-        else
-        {
+void dequeue() {
+    if (front == -1) {
+        printf("Queue is empty (Underflow)\n");
+    } else {
+        printf("Deleted item: %d from index %d\n", q[front], front);
+        if (front == rear) {
+            front = rear = -1;
+        } else {
             front = (front + 1) % SIZE;
         }
     }
 }
 
-void display()
-{
-    if (front == -1 && rear == -1)
-    {
+void display() {
+    if (front == -1) {
         printf("Queue is empty\n");
-        return;
+    } else {
+        printf("Queue elements: ");
+        int i = front;
+        while (1) {
+            printf("%d ", q[i]);
+            if (i == rear)
+                break;
+            i = (i + 1) % SIZE;
+        }
+        printf("\n");
     }
-
-    printf("Queue elements are: ");
-    if (front <= rear)
-    {
-        for (int i = front; i <= rear; i++)
-            printf("%d ", Q[i]);
-    }
-    else
-    {
-        for (int i = front; i < SIZE; i++)
-            printf("%d ", Q[i]);
-        for (int i = 0; i <= rear; i++)
-            printf("%d ", Q[i]);
-    }
-    printf("\n");
 }
 
-int main()
-{
+int main() {
     int choice, item;
-    do
-    {
-        printf("\nEnter your choice:\n");
-        printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
-        scanf("%d", &choice);
 
-        switch (choice)
-        {
-        case 1:
-            printf("Enter the item to be inserted: ");
-            scanf("%d", &item);
-            enqueue(item);
-            break;
-        case 2:
-            dequeue();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            printf("Exiting...\n");
-            break;
-        default:
-            printf("Invalid choice\n");
+    printf("--- Circular Queue Implementation (Size: %d) ---\n", SIZE);
+
+    do {
+        printf("\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input! Please enter a number.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        switch (choice) {
+            case 1:
+                printf("Enter item to insert: ");
+                if (scanf("%d", &item) != 1) {
+                    printf("Invalid input! Try again.\n");
+                    while (getchar() != '\n');
+                    break;
+                }
+                enqueue(item);
+                break;
+
+            case 2:
+                dequeue();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
         }
     } while (choice != 4);
 
